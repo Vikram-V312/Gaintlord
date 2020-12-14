87b52fa103a199815864786346662d23d9a4dea3
Vikram-V312 <75898981+Vikram-V312@users.noreply.github.com>
2020-12-14 19:46:32 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,7 @@
+marks = int(input("Enter the marks : "))
+if marks>=90:
+    print("You got'A'grade")
+elif marks>=60:
+    print("You got'B'grade")
+else:
+    print("You can score better")
---
+++ while statement.py
@@ -0,0 +1,19 @@
+num  = int(input(" Please Enter Any Number: "))
+
+Number = 1
+
+print("Prime numbers between", 1, "and", num, "are:")
+
+while Number <= num:
+    count = 0
+    i = 2
+
+    while i <= Number // 2:
+        if Number % i == 0:
+            count = count + 1
+            break
+        i = i + 1
+
+    if count == 0 and Number != 1:
+        print(" %d" % Number, end='  ')
+    Number = Number + 1
